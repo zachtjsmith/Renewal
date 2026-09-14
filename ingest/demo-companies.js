b2e56ea28ad8a3f73e43667de1798f052c0a5bd7
zachtjsmith <zachtjsmith@gmail.com>
2026-09-14 15:57:30 -0700

    Delete ingest directory

--- ingest/demo-companies.js
+++
@@ -1,6 +0,0 @@
-export const DEMO = [
-  { ats: "greenhouse", token: "northloop",  name: "Northloop",  industry: "Developer tools",    site: "https://northloop.example" },
-  { ats: "lever",      token: "parcelay",   name: "Parcelay",   industry: "Logistics software", site: "https://parcelay.example" },
-  { ats: "ashby",      token: "havenpoint", name: "Havenpoint", industry: "Insurtech",          site: "https://havenpoint.example" },
-  { ats: "workable",   token: "quillstone", name: "Quillstone", industry: "Legal tech",         site: "https://quillstone.example" },
-];
