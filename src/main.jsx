7053d9e6a467869dec82fd9b881bc873de64fe2c
zachtjsmith <zachtjsmith@gmail.com>
2026-09-14 15:57:39 -0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
-const el = document.getElementById("root");
-if (!el) throw new Error('No #root element — check index.html');
-createRoot(el).render(<React.StrictMode><App /></React.StrictMode>);
